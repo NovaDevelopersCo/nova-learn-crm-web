203e2b775fa782b4fefd02ea28f57a164d66bcf4
Ivan <ivan.golubkov7@gmail.com>
2024-03-03 19:53:16 +0200

    made a mobile version

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 import { Avatar, Button } from '@shared/ui'
 
@@ -20,6 +20,8 @@ import settings from '/public/icons/settings.png'
 import teams from '/public/icons/teams.png'
 
 const Sidebar: FC = () => {
+	const [isSidebarVisible, setIsSidebarVisible] = useState(true)
+
 	useEffect(() => {
 		const setSidebarWidth = () => {
 			const sidebar = document.querySelector<HTMLElement>(
@@ -27,8 +29,9 @@ const Sidebar: FC = () => {
 			)
 			if (sidebar) {
 				const windowWidth = window.innerWidth
-				const isNarrowScreen = windowWidth < 451
+				const isNarrowScreen = windowWidth < 1025
 				sidebar.style.width = isNarrowScreen ? '100%' : '310px'
+				setIsSidebarVisible(!isNarrowScreen)
 			}
 		}
 
@@ -40,70 +43,83 @@ const Sidebar: FC = () => {
 		}
 	}, [])
 
+	const toggleSidebarVisibility = () => {
+		setIsSidebarVisible(!isSidebarVisible)
+	}
+
 	return (
-		<div className={style.sidebar}>
-			<div className={style.button}>
-				<Button icon={people.src} className={style.customButton} />
-				<Button icon={dot.src} className={style.customButton} />
-			</div>
-			<div className={style.avatar}>
-				<Avatar level={5} experience={100} />
-			</div>
-			<div className={style.clusterbtn}>
-				<Button
-					icon={dash.src}
-					label='Dashboard'
-					className={style.customcluster}
-				/>
-				<Button
-					icon={calendars.src}
-					label='Calendars'
-					className={style.customcluster}
-				/>
-				<Button
-					icon={invoice.src}
-					label='Invoice'
-					className={style.customcluster}
-				/>
-				<Button
-					icon={files.src}
-					label='Files'
-					className={style.customcluster}
-				/>
-				<Button
-					icon={events.src}
-					label='Events'
-					className={style.customcluster}
-				/>
-				<Button
-					icon={teams.src}
-					label='Teams'
-					className={style.customcluster}
-				/>
-				<Button
-					icon={massage.src}
-					label='Massage'
-					className={style.customcluster}
-				/>
-				<Button
-					icon={settings.src}
-					label='Settings'
-					className={style.customcluster}
-				/>
-			</div>
-			<div className={style.twobuton}>
-				<Button
-					icon={feedback.src}
-					label='Send Feedback'
-					className={style.twobtn}
-					rightIcon={rightIcon.src}
-				/>
-				<Button
-					icon={knowledge.src}
-					label='Knowledge Base'
-					className={style.twobtn}
-					rightIcon={rightIcon.src}
-				/>
+		<div>
+			<Button
+				className={style.toggleBtn}
+				onClick={toggleSidebarVisibility}
+				label={isSidebarVisible ? 'Hide Sidebar' : 'Show Sidebar'}
+			/>
+			<div
+				className={`${style.sidebar} ${isSidebarVisible ? style.visible : style.hidden}`}
+			>
+				<div className={style.button}>
+					<Button icon={people.src} className={style.customButton} />
+					<Button icon={dot.src} className={style.customButton} />
+				</div>
+				<div className={style.avatar}>
+					<Avatar level={5} experience={100} />
+				</div>
+				<div className={style.clusterbtn}>
+					<Button
+						icon={dash.src}
+						label='Dashboard'
+						className={style.customcluster}
+					/>
+					<Button
+						icon={calendars.src}
+						label='Calendars'
+						className={style.customcluster}
+					/>
+					<Button
+						icon={invoice.src}
+						label='Invoice'
+						className={style.customcluster}
+					/>
+					<Button
+						icon={files.src}
+						label='Files'
+						className={style.customcluster}
+					/>
+					<Button
+						icon={events.src}
+						label='Events'
+						className={style.customcluster}
+					/>
+					<Button
+						icon={teams.src}
+						label='Teams'
+						className={style.customcluster}
+					/>
+					<Button
+						icon={massage.src}
+						label='Massage'
+						className={style.customcluster}
+					/>
+					<Button
+						icon={settings.src}
+						label='Settings'
+						className={style.customcluster}
+					/>
+				</div>
+				<div className={style.twobuton}>
+					<Button
+						icon={feedback.src}
+						label='Send Feedback'
+						className={style.twobtn}
+						rightIcon={rightIcon.src}
+					/>
+					<Button
+						icon={knowledge.src}
+						label='Knowledge Base'
+						className={style.twobtn}
+						rightIcon={rightIcon.src}
+					/>
+				</div>
 			</div>
 		</div>
 	)
