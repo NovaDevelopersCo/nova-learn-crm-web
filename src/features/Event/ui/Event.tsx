78df483d465ffd55205682baf94da3fbbc8e013e
Yaroslavl1s <lisinskijaroslav@gmail.com>
2024-03-09 23:34:00 +0300

    add style for event calendar

--- src/features/Event/ui/Event.tsx
+++ src/features/Event/ui/Event.tsx
@@ -11,70 +11,30 @@ import Icon from '/public/icons/Icon.png'
 
 const Event = () => {
 	return (
-		<div>
-			<div className={style.input}>
-				<div className={style.inputWithIcon}>
-					<input
-						type='text'
-						placeholder='Add Place'
-						className={style.icone}
-					/>
-					<img
-						src={Icon.src}
-						alt='Icon'
-						className={style.inputIcon}
-					/>
-				</div>
-				<div className={style.inputWithIcon}>
-					<input
-						type='text'
-						placeholder='Add Date'
-						className={style.icone}
-					/>
-					<img
-						src={Icon1.src}
-						alt='Icon1'
-						className={style.inputIcon}
-					/>
-				</div>
-				<div className={style.inputWithIcon}>
-					<input
-						type='text'
-						placeholder='Add Members'
-						className={style.icone}
-					/>
-					<img
-						src={Icon2.src}
-						alt='Icon2'
-						className={style.inputIcon}
-					/>
-				</div>
-				<div className={style.inputWithIcon}>
-					<input
-						type='text'
-						placeholder='Add Notes'
-						className={style.icone}
-					/>
-					<img
-						src={Icon3.src}
-						alt='Icon3'
-						className={style.inputIcon}
-					/>
-				</div>
-				<div className={style.inputWithIcon}>
-					<input
-						type='text'
-						placeholder='Add Time'
-						className={style.icone}
-					/>
-					<img
-						src={Icon4.src}
-						alt='Icon4'
-						className={style.inputIcon}
-					/>
-				</div>
-			</div>
-		</div>
+		<div className={style.inputtext}>
+    <div className={style.inputContainer}>
+        <div className={style.inputWithIcon}>
+            <img src={Icon.src} alt="Icon" className={style.inputIcon} />
+            <input type="text" placeholder="Add Place" className={style.icone} />
+        </div>
+        <div className={style.inputWithIcon}>
+            <img src={Icon1.src} alt="Icon1" className={style.inputIcon} />
+            <input type="text" placeholder="Add Date" className={style.icone} />
+        </div>
+        <div className={style.inputWithIcon}>
+            <img src={Icon2.src} alt="Icon2" className={style.inputIcon} />
+            <input type="text" placeholder="Add Members" className={style.icone} />
+        </div>
+        <div className={style.inputWithIcon}>
+            <img src={Icon3.src} alt="Icon3" className={style.inputIcon} />
+            <input type="text" placeholder="Add Notes" className={style.icone} />
+        </div>
+        <div className={style.inputWithIcon}>
+            <img src={Icon4.src} alt="Icon4" className={style.inputIcon} />
+            <input type="text" placeholder="Add Time" className={style.icone} />
+        </div>
+    </div>
+</div>
 	)
 }
 
