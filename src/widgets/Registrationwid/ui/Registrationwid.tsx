cbff7624119d9a9f7898be55ee1371564be36664
Ivan <ivan.golubkov7@gmail.com>
2024-03-17 19:00:23 +0200

    Saving data to the console

--- src/widgets/Registrationwid/ui/Registrationwid.tsx
+++ src/widgets/Registrationwid/ui/Registrationwid.tsx
@@ -6,10 +6,50 @@ import style from './Registrationwid.module.scss'
 
 const Registrationwid = () => {
 	const [passwordVisible, setPasswordVisible] = useState(false)
+	const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [agreedToTerms, setAgreedToTerms] = useState(false);
 
 	const togglePasswordVisibility = () => {
 		setPasswordVisible(!passwordVisible)
 	}
+
+	const handleInputChange = (event: { target: { name: any; value: any; type: any; checked: any } }) => {
+		const { name, value, type, checked } = event.target;
+		const newValue = type === 'checkbox' ? checked : value;
+		switch (name) {
+			case 'fullName':
+				setFullName(newValue);
+				break;
+			case 'email':
+				setEmail(newValue);
+				break;
+			case 'password':
+				setPassword(newValue);
+				break;
+			case 'phoneNumber':
+				setPhoneNumber(newValue);
+				break;
+			case 'agreedToTerms':
+				setAgreedToTerms(newValue);
+				break;
+			default:
+				break;
+		}
+	}
+
+	const handleSubmit = () => {
+        const formData = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phoneNumber: phoneNumber,
+            agreedToTerms: agreedToTerms
+        };
+        console.log(formData);
+    }
 
 	return (
 		<div className={style.registration}>
@@ -20,14 +60,17 @@ const Registrationwid = () => {
 			<div className={style.form_container}>
 				<form action='' className={style.form}>
 					<label className={style.label}>Full Name</label>
-					<input type='text' className={style.input} />
+					<input type='text' name='fullName' className={style.input} value={fullName} onChange={handleInputChange} />
 					<label className={style.label}>Email Address</label>
-					<input type='email' className={style.input} />
+					<input type='email' name='email' className={style.input} value={email} onChange={handleInputChange} />
 					<label className={style.label}>Password</label>
 					<div className={style.show_hide_password}>
 						<input
 							type={passwordVisible ? 'text' : 'password'}
+							name='password'
 							className={style.input}
+							value={password} 
+							onChange={handleInputChange}
 						/>
 						<button
 							className={style.show_hide_button}
@@ -38,21 +81,23 @@ const Registrationwid = () => {
 						</button>
 					</div>
 					<label className={style.label}>Phone Number</label>
-					<input type='phone' className={style.input} />
+					<input type='phone' name='phoneNumber' className={style.input} value={phoneNumber} onChange={handleInputChange} />
 					<label
 						htmlFor='privacyCheckbox'
 						className={style.checkbox_label}
 					>
 						<input
 							type='checkbox'
+							name='agreedToTerms'
 							id='privacyCheckbox'
 							className={style.checkbox}
+							onChange={handleInputChange}
 						/>
 						I agree to the Terms of Service and Privacy Policy
 					</label>
 				</form>
 				<div className={style.btnregdiv}>
-					<button className={style.btnreg}>
+					<button className={style.btnreg} onClick={handleSubmit}>
 						<span>Create Account</span>
 					</button>
 				</div>
